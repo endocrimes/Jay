18b36075b30abf5c01b3100684b50e15314069b2
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-02-21 23:56:09 +0000

    fixes for linux

--- Sources/JayExample/main.swift
+++ Sources/JayExample/main.swift
@@ -3,13 +3,13 @@ import Foundation
 
 func tryParsing() {
 
-    let data = Array("{\t\"hello\" : \"wor🇨🇿ld\", \n\t \"val\": 1234, \"many\": [\n-12.32, null, \"yo\"\r], \"emptyDict\": {}, \"dict\": {\"arr\":[]}, \"name\": true}".utf8)
+    let data = Array("{\t\"hello\" : \"wor🇨🇿ld\", \n\t \"val\": 1234, \"many\": [\n-12.32, \"yo\"\r], \"emptyDict\": {}, \"dict\": {\"arr\":[]}, \"name\": true}".utf8)
     let exp: [String: Any] = [
         "hello": "wor🇨🇿ld",
         "val": 1234,
         "many": [
             -12.32,
-            NSNull(),
+            // null, add NSNull back once we can reliably compare object hierarchies
             "yo"
         ] as [Any],
         "emptyDict": [String:Any](),
@@ -23,7 +23,7 @@ func tryParsing() {
     let json = try! Jay().jsonFromData(data)
     let retStr = "\(json)"
 
-    assert(expStr == retStr)
+    assert(expStr == retStr, "Mismatch: Expected:\n\(expStr)\nReturned:\n\(retStr)")
     print("Parsing works")
 }
 
@@ -38,17 +38,17 @@ func tryFormatting() {
             NSNull(),
             "yo"
         ] as [Any],
-        "emptyDict": [String:Any](),
+        "emptyDict": [String: Any](),
         "dict": [
             "arr": [Int]()
-        ],
+        ] as [String: Any],
         "name": true
     ]
 
     let dataOut = try! Jay().dataFromJson(obj)
     let retStr = try! dataOut.string()
 
-    assert(origStr == retStr)
+    assert(origStr == retStr, "Mismatch: Expected:\n\(expStr)\nReturned:\n\(retStr)")
     print("Formatting works")
 }
 
