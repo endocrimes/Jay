3923af7aa471d746592ad83e0cbef80f32110479
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-02-26 10:18:10 +0000

    force create a dylib

--- Package.swift
+++ Package.swift
@@ -15,3 +15,10 @@ let package = Package(
         )
     ]
 )
+
+//with the new swiftpm we have to force it to create a static lib so that we can use it
+//from xcode. this will become unnecessary once official xcode+swiftpm support is done.
+//watch progress: https://github.com/apple/swift-package-manager/compare/xcodeproj?expand=1
+
+let libJay = Product(name: "Jay", type: .Library(.Dynamic), modules: "Jay")
+products.append(libJay)
