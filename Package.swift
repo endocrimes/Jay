9217c585f52a15049fcc16055fe608d1152ed6f1
Tanner <me@tanner.xyz>
2016-09-08 20:31:28 -0400

    remove examples (#48)
    [Project] Remove examples

--- Package.swift
+++ Package.swift
@@ -3,7 +3,11 @@ import PackageDescription
 let package = Package(
     name: "Jay",
     targets: [
-        Target(name: "JayExample", dependencies: ["Jay"]),
-        Target(name: "JayPerformance", dependencies: ["Jay"])
+        // Target(name: "JayExample", dependencies: ["Jay"]),
+        // Target(name: "JayPerformance", dependencies: ["Jay"])
+    ],
+    exclude: [
+        "Sources/JayExample",
+        "Sources/JayPerformance",
     ]
 )
